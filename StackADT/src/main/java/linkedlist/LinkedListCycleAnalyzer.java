43fd3b4daaf17c1e659c73b591ade0cede842a54
egoldfar <egoldfar@student.touro.edu>
2025-10-05 22:37:34 -0400

    completed HW
    implemented the method and the unit tests passes

--- StackADT/src/main/java/linkedlist/LinkedListCycleAnalyzer.java
+++ StackADT/src/main/java/linkedlist/LinkedListCycleAnalyzer.java
@@ -5,7 +5,46 @@ import support.CycleInfo;
 import support.LLNode;
 
 public class LinkedListCycleAnalyzer<T> {
-    public static <T> CycleInfo detectCycleInfo(LLNode<T> head){
-        throw new NotImplementedException("TODO: implement this method");
+    public static <T> CycleInfo detectCycleInfo(LLNode<T> head) {
+
+       LLNode <T> h = head;
+       LLNode<T> t = head.getLink();
+        boolean noLoop = false;
+
+        while(h != t && !noLoop) {
+          try {
+              h = h.getLink();
+              t = t.getLink().getLink();
+          }catch(NullPointerException e) {
+              noLoop = true;
+          }
+        }
+
+        if(noLoop) {
+            return new CycleInfo(-1, 0);
+        }
+
+        int i = 0;
+        do{
+            h = h.getLink();
+            i++;
+        }while (h != t);
+
+        int index = -1;
+
+        do {
+            if (index != -1) {
+                head = head.getLink();
+            }
+
+            h = head;
+
+            for (int j = 0; j < i; j++) {
+                h = h.getLink();
+            }
+            index++;
+        } while(head != h);
+
+        return new CycleInfo(index, i);
     }
 }
